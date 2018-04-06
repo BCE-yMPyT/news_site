75c863deedcf96c809ccb6ded4be0b7749ff0e57
admin <askorytin94@gmail.com>
2018-04-06 10:10:26 +0300

    Add so much stuff :3

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 
 def index(request):
+	print("request to index")
 	return render(request, 'mainApp/homePage.html')
 # Create your views here.
 def contact(request):
-	return render(request, 'mainApp/basic.html', {'values': ['Если есть вопросы, задавай из по телефону', 
-	'8800-555-35-35', 'example@mail.ru']})
+	print("request to index")
+	return render(request, 'mainApp/contacts.html')
